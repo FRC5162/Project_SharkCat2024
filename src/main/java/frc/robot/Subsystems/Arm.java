b5d113fff09fd4aa6a5d8cf87cf3cf070c3c1404
ncgears <1918ncgears@gmail.com>
2024-03-01 11:09:13 -0500

    removed unused imports

--- src/main/java/frc/robot/Subsystems/Arm.java
+++ src/main/java/frc/robot/Subsystems/Arm.java
@@ -7,14 +7,10 @@ package frc.robot.Subsystems;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
-import com.revrobotics.CANSparkBase;
 import com.revrobotics.CANSparkMax;
-import com.revrobotics.RelativeEncoder;
 import com.revrobotics.CANSparkLowLevel.MotorType;
-import com.revrobotics.CANSparkLowLevel;
 
 import edu.wpi.first.wpilibj.DigitalInput;
-import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 
 public class Arm extends SubsystemBase {
   /** Creates a new Arm. */
